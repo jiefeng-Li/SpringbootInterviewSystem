f5d23565b53cc24aa6c6f6bd6b06683f240b85f0
jiefeng-Li <jiefengli2022@outlook.com>
2026-01-07 22:10:06 +0800

    后端项目创建

---
+++ backend/InterviewSys/src/main/java/com/cuit/interviewsystem/InterviewSysApplication.java
@@ -0,0 +1,13 @@
+package com.cuit.interviewsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewSysApplication.class, args);
+    }
+
+}
